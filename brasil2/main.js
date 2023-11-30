926592a639a214f9525ed8519ae70e1bd0321a32
jiajiechen2003 <jchen2223@politecnics.barcelona>
2023-11-30 20:17:11 +0100

    topo verde + puntos / topo rojo - puntos

--- brasil2/main.js
+++ brasil2/main.js
@@ -27,26 +27,43 @@ function imatgeCursor() {
 function asomaTopos() {
   for (let i = 1; i <= 9; i++) {
     const topo = document.getElementById("imatge" + i);
+    const topoVerde = document.getElementById("imatge1" + i);
+    const topoRojo = document.getElementById("imatge2" + i);
+
     topo.style.display = "none";
+    topoVerde.style.display = "none";
+    topoRojo.style.display = "none";
   }
+
   if (tempsRestant > 0) {
-    const numTopos = Math.floor(Math.random() * 3) + 1;
+    const numTopos = Math.floor(Math.random() * 4) + 1;
 
     for (let i = 0; i < numTopos; i++) {
       let num;
-      do {
+      // do {
         num = Math.floor(Math.random() * 9) + 1;
-      } while (
-        document.getElementById("imatge" + num).style.display === "block"
-      );
+      // } while (
+      //   document.getElementById("imatge" + num).style.display === "block"
+      // );
 
-      const topo = document.getElementById("imatge" + num);
-      topo.style.display = "block";
+      const numAleatorio = Math.random();
+
+      if (numAleatorio < 0.1) {
+        const topoVerde = document.getElementById("imatge1" + num);
+        topoVerde.style.display = "block";
+      } else if (numAleatorio < 0.2) {
+        const topoRojo = document.getElementById("imatge2" + num);
+        topoRojo.style.display = "block";
+      } else {
+        const topo = document.getElementById("imatge" + num);
+        topo.style.display = "block";
+      }
     }
-    
+
     setTimeout(asomaTopos, 1300);
   }
 }
+
 
 function compteEnrere() {
   const minuts = Math.floor(tempsRestant / 60);
@@ -64,20 +81,48 @@ function compteEnrere() {
 
 function sumarPunts() {
   let puntsInicial = 0;
-
+  
   for (let i = 1; i <= 9; i++) {
     const topo = document.getElementById("imatge" + i);
+    const topoVerde = document.getElementById("imatge1" + i);
+    const topoRojo = document.getElementById("imatge2" + i);
 
     topo.addEventListener("click", () => {
       if (tempsRestant > 0) {
         puntsInicial += 10;
         const punts = document.getElementById("puntuacio");
         punts.innerHTML = `${puntsInicial}`;
-        topo.src = "img/mole-whacked.png"
+        topo.src = "img/topo-golpeado.png"
         setTimeout(() => {
           topo.style.display = "none";
           topo.src = "img/topo.png";
-        }, 500);
+        }, 200);
+      }
+    });
+
+    topoVerde.addEventListener("click", () => {
+      if (tempsRestant > 0) {
+        puntsInicial += 20;
+        const punts = document.getElementById("puntuacio");
+        punts.innerHTML = `${puntsInicial}`;
+        topoVerde.src = "img/topo-golpeado-verde.png"
+        setTimeout(() => {
+          topoVerde.style.display = "none";
+          topoVerde.src = "img/topoVerde.png";
+        }, 200);
+      }
+    });
+
+    topoRojo.addEventListener("click", () => {
+      if (tempsRestant > 0) {
+        puntsInicial -= 10;
+        const punts = document.getElementById("puntuacio");
+        punts.innerHTML = `${puntsInicial}`;
+        topoRojo.src = "img/topo-golpeado-rojo.png"
+        setTimeout(() => {
+          topoRojo.style.display = "none";
+          topoRojo.src = "img/topoRojo.png";
+        }, 200);
       }
     });
   }
@@ -91,3 +136,6 @@ function musicaFons() {
   audio.playbackRate = 1.5;
   audio.play();
 }
+
+
+
